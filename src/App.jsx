ebd2cb18bd71fa7a4adf90c1ab70be341e95191f
Matt Lilley <m.k.lilley@gmail.com>
2023-04-09 12:28:24 +0100

    Adds TopBar to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Cards from "./components/Cards";
+import TopBar from "./components/TopBar";
 
 function App() {
   return (
     <>
+      <TopBar></TopBar>
       <Cards></Cards>
     </>
   );
