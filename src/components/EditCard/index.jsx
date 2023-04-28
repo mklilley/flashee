9bb8feefa98c06c9b3e58593af4e822e2cfc4463
Matt Lilley <m.k.lilley@gmail.com>
2023-04-28 22:30:43 +0100

    Adds MathLive component to the editCard component

--- src/components/EditCard/index.jsx
+++ src/components/EditCard/index.jsx
@@ -4,10 +4,14 @@ import Modal from "../Modal";
 
 import { db } from "../../services/storage";
 
+import MathLive from "../MathLive";
+
 // This component can be used to update an existing card of create a new card
 function EditCard({ close, card, setReloadCards }) {
   const [question, setQuestion] = useState(card.question);
   const [answer, setAnswer] = useState(card.answer);
+  const [showMathQuestion, setShowMathQuestion] = useState(false);
+  const [showMathAnswer, setShowMathAnswer] = useState(false);
 
   function handleQuestionChange(e) {
     setQuestion(e.target.value);
@@ -48,6 +52,19 @@ function EditCard({ close, card, setReloadCards }) {
     setReloadCards((prev) => prev + 1);
     close();
   }
+
+  function toggleMathQuestion() {
+    setShowMathQuestion((prev) => !prev);
+  }
+
+  function toggleMathAnswer() {
+    setShowMathAnswer((prev) => !prev);
+  }
+
+  function stripKatexDollars(text) {
+    return text.replace(/^\$\$|\$\$$/g, "");
+  }
+
   return (
     <Modal close={close}>
       <form className="flashcard-form misc" onSubmit={saveCard}>
@@ -56,15 +73,39 @@ function EditCard({ close, card, setReloadCards }) {
           value={question}
           onChange={handleQuestionChange}
           required></input>
+        <span className="math" onClick={toggleMathQuestion}>
+          f(x)
+        </span>
+
+        <br />
+        <br />
         <input
           placeholder="Answer"
           value={answer}
           onChange={handleAnswerChange}
           required></input>
+        <span className="math" onClick={toggleMathAnswer}>
+          f(x)
+        </span>
+
         <br />
         <br />
         <button type="submit">Save Card</button>
       </form>
+      {showMathAnswer && (
+        <MathLive
+          latex={stripKatexDollars(answer)}
+          saveMath={setAnswer}
+          close={toggleMathAnswer}
+        />
+      )}
+      {showMathQuestion && (
+        <MathLive
+          latex={stripKatexDollars(question)}
+          saveMath={setQuestion}
+          close={toggleMathQuestion}
+        />
+      )}
     </Modal>
   );
 }
