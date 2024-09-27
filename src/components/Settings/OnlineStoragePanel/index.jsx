c35da5260548acc986309c51991328ecc85233c7
Matt Lilley <m.k.lilley@gmail.com>
2024-09-27 08:19:35 +0100

    Adds OnlineStoragePanel component

---
+++ src/components/Settings/OnlineStoragePanel/index.jsx
@@ -0,0 +1,51 @@
+import Panel from "../../Panel"
+
+function OnlineStoragePanel({ children }) {
+
+  return (
+      <Panel heading="Online storage" color="blue">
+          <span className="error"> Problem with online storage</span>
+          <br />
+          <div>
+            <label className="switch">
+              Toggle online storage
+              <input type="checkbox" />
+              <span className="slider round"></span>
+            </label>
+            <br />
+            <br />
+          </div>
+
+          <label className="switch">
+            Toggle sync warnings
+            <input type="checkbox" />
+            <span className="slider round"></span>
+            <br />
+            <br />
+          </label>
+
+          <div>
+            My storage box ID:
+            <br />
+            <strong>boxID</strong> <button>copy</button>
+            <br />
+            <br />
+            My storage box key:
+            <br />
+            <strong>apiKey</strong> <button>copy</button>
+            <br />
+            <br />
+            <button>Switch to another storage box</button>
+            <br />
+            <br />
+            <div>
+              <button>Switch back to my storage box</button>
+              <br />
+              <br />
+            </div>
+          </div>
+      </Panel>
+  );
+}
+
+export default OnlineStoragePanel;
