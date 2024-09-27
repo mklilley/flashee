fdca08b4b4abf99634e7bb7e1cfc045642176ce1
Matt Lilley <m.k.lilley@gmail.com>
2024-09-27 08:18:33 +0100

    Fixes Panel component header

--- src/components/Panel/index.jsx
+++ src/components/Panel/index.jsx
@@ -3,7 +3,7 @@ function Panel({ children, heading, color }) {
   return (
 
       <div className={`panel ${color}`}>
-        <h3>{heading}</h3>
+        <h3 className="open">{heading}</h3>
         <div className="items">
           {children}
         </div>
