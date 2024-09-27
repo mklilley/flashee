04a588b8e43bc3b2b21dce605fbfb4b56e680658
Matt Lilley <m.k.lilley@gmail.com>
2024-09-27 08:20:06 +0100

    Refactors settings to use panel components

--- src/components/Settings/index.jsx
+++ src/components/Settings/index.jsx
@@ -1,100 +1,24 @@
 import Modal from "../Modal";
 
+import YourDataPanel from "./YourDataPanel";
+import OnlineStoragePanel from "./OnlineStoragePanel";
+import HelpPanel from "./HelpPanel";
+
 // import { db } from "../../services/storage";
+
 
 function Settings({ close, totalNumberOfCards }) {
   return (
     <Modal close={close}>
-      <div className="settings">
+      <div>
         <h2> Settings </h2>
         <div className="left">
-          <div className="your-data">
-            <h3>Your data</h3>
-            <div className="items">
-              Number of cards: <strong>{totalNumberOfCards}</strong> <br />
-              <br />
-              <div>
-                <button>Download your data</button>
-                <br />
-                <br />
-              </div>
-              <div>
-                <button>Delete all your data</button>
-                <br />
-                <br />
-              </div>
-              <div>
-                <button>Import data from file</button>
-              </div>
-            </div>
-          </div>
+          <YourDataPanel totalNumberOfCards={totalNumberOfCards}></YourDataPanel>
           <br />
-
-          <div className="online-storage ">
-            <h3>Online storage</h3>
-            <div className="items">
-              <span className="error"> Problem with online storage</span>
-              <br />
-              <div>
-                <label className="switch">
-                  Toggle online storage
-                  <input type="checkbox" />
-                  <span className="slider round"></span>
-                </label>
-                <br />
-                <br />
-              </div>
-
-              <label className="switch">
-                Toggle sync warnings
-                <input type="checkbox" />
-                <span className="slider round"></span>
-                <br />
-                <br />
-              </label>
-
-              <div>
-                My storage box ID:
-                <br />
-                <strong>boxID</strong> <button>copy</button>
-                <br />
-                <br />
-                My storage box key:
-                <br />
-                <strong>apiKey</strong> <button>copy</button>
-                <br />
-                <br />
-                <button>Switch to another storage box</button>
-                <br />
-                <br />
-                <div>
-                  <button>Switch back to my storage box</button>
-                  <br />
-                  <br />
-                </div>
-              </div>
-            </div>
-          </div>
+          <OnlineStoragePanel></OnlineStoragePanel>
           <br />
-
-          <div className="misc ">
-            <h3>Help</h3>
-            <div className="items">
-              <button>Send Feedback</button>
-              <br />
-              <br />
-              <button>Show welcome screen again</button>
-              <br />
-              <br />
-              <div>
-                <button>Share my cards</button>
-                <br />
-                <br />
-              </div>
-              <button>Reset App</button> <br />
-              <br />
-            </div>
-          </div>
+          <HelpPanel></HelpPanel>
+          <br />
         </div>
         <br />
         <a
