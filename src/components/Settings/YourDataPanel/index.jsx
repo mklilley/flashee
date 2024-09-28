15cdc0f6ce80423b6849ced6ad2e8e15777248e6
Matt Lilley <m.k.lilley@gmail.com>
2024-09-28 09:02:59 +0100

    Implements download of data

--- src/components/Settings/YourDataPanel/index.jsx
+++ src/components/Settings/YourDataPanel/index.jsx
@@ -2,12 +2,41 @@ import Panel from "../../Panel"
 
 function YourDataPanel({ totalNumberOfCards }) {
 
+  async function downloadData(){
+    //  Adapted from https://ourcodeworld.com/articles/read/189/how-to-create-a-file-and-generate-a-download-with-javascript-in-the-browser-without-a-server
+    
+    // Load cards from the database
+    const cards = await db.read({});
+
+    // Remove card data relating to how many times the card has been read, the difficulty, etc
+    // and just retain the question and answer
+    let dataForDownload = cards.map(function(card) {
+      return { question: card.question, answer: card.answer };
+    });
+
+    const element = document.createElement("a");
+    element.setAttribute(
+      "href",
+      "data:text/plain;charset=utf-8," +
+        encodeURIComponent(JSON.stringify(dataForDownload))
+    );
+    element.setAttribute("download", "flashee.json");
+
+    element.style.display = "none";
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+  }
+
+
   return (
       <Panel heading="Your data" color="yellow">
           Number of cards: <strong>{totalNumberOfCards}</strong> <br />
           <br />
           <div>
-            <button>Download your data</button>
+            <button onClick={downloadData}>Download your data</button>
             <br />
             <br />
           </div>
