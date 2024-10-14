435416ba06e69f12418fc9c7ceb0135c1d3fc68b
Matt Lilley <m.k.lilley@gmail.com>
2024-10-14 07:22:02 +0100

    Updates Modal component to portal to document body

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,10 +1,13 @@
+import { createPortal } from 'react-dom';
+
 import styles from "./styles.module.css";
 
 function Modal({ children, close }) {
+  const modalRoot = document.body
   function stop(e) {
     e.stopPropagation();
   }
-  return (
+  return createPortal(
     <div className={styles["modal-mask"]}>
       <div className={styles["modal-wrapper"]} onClick={close}>
         <div className={styles["modal-container"]} onClick={stop}>
@@ -14,8 +17,8 @@ function Modal({ children, close }) {
           <div className={styles["modal-body"]}>{children}</div>
         </div>
       </div>
-    </div>
-  );
+    </div>,
+  modalRoot);
 }
 
 export default Modal;
