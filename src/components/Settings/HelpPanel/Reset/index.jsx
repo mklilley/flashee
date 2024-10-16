4aac23a31e9c53dbcbd8604e2f201b86ed725c5a
Matt Lilley <m.k.lilley@gmail.com>
2024-10-16 09:13:39 +0100

    Implements reset component functionality

--- src/components/Settings/HelpPanel/Reset/index.jsx
+++ src/components/Settings/HelpPanel/Reset/index.jsx
@@ -1,11 +1,23 @@
 import Modal from "../../../Modal";
 
-
 function Reset({ close }) {
 
+  function resetApp(){
+    localStorage.clear();
+    location.reload();
+  }
+
+
   return (
-    <Modal close={close}>
-      <h2>Reset</h2>
+    <Modal close={close} color="purple">
+      <h2>Reset App</h2>
+      Resetting the app will delete all local data and give you new online
+      storage credentials. Your online data will persist, but you will lose
+      access to it unless you have taken note of your storage ID and key. <br />
+      <br />
+      <button onClick={resetApp}>Yes, reset app</button> <br />
+      <br />
+      <button onClick={close}>No, take me back</button>
     </Modal>
   );
 }
