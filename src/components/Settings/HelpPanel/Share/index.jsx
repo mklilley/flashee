63ee8689a0552e3c4408ac63d56765ca9d883ca7
Matt Lilley <m.k.lilley@gmail.com>
2024-10-21 10:13:15 +0100

    Implements share component

--- src/components/Settings/HelpPanel/Share/index.jsx
+++ src/components/Settings/HelpPanel/Share/index.jsx
@@ -1,11 +1,54 @@
 import Modal from "../../../Modal";
-
+import { useState } from "react";
 
 function Share({ close }) {
+  const [boxID, setBoxID] = useState("1234");
+  const [copiedText, setCopiedText] = useState(null);
+
+  function copyToClipboard(text) {
+    navigator.clipboard.writeText(text).then(
+      () => {
+        console.log("Async: Copying to clipboard was successful!");
+        setCopiedText(text);
+        setTimeout(() => setCopiedText(null), 1000);
+      },
+      (err) => {
+        console.error("Async: Could not copy text: ", err);
+      }
+    );
+  }
 
   return (
-    <Modal close={close}>
+    <Modal close={close} color="purple">
       <h2>Share</h2>
+      To share your cards with others, give them your storage box ID:
+      <br />
+      <br />
+      <strong>{boxID}</strong>{" "}
+      <button
+        className={copiedText === boxID ? "copied" : ""}
+        onClick={() => copyToClipboard(boxID)}
+      >
+        Copy
+      </button>
+      <br />
+      <br />
+      They can then use the "Switch to another storage box" button in the
+      "Online Storage" settings. This is best for those who already have Flashee
+      installed.
+      <br /> <br />
+      Alternatively, anyone can see your cards via this link:
+      <br /> <br />
+      <strong>https://flashee.lilley.io?box={boxID}</strong>{" "}
+      <button
+        className={copiedText === `https://flashee.lilley.io?box=${boxID}` ? "copied" : ""}
+        onClick={() => copyToClipboard(`https://flashee.lilley.io?box=${boxID}`)}
+      >
+        Copy
+      </button>
+      <br />
+      <br />
+      <button onClick={close}>OK</button>
     </Modal>
   );
 }
