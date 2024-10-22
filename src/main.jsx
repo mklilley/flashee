f04c8fe8a9d14085b8425ebc118422818aeef1b0
Matt Lilley <m.k.lilley@gmail.com>
2024-10-22 08:51:14 +0100

    Wraps App in Recoil

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { RecoilRoot } from 'recoil';
 
 import { MathfieldElement } from "mathlive";
 MathfieldElement.fontsDirectory = "/assets/fonts";
 MathfieldElement.soundsDirectory = null;
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>
+);
