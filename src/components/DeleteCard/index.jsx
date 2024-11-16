1bf2ff361c1c02064530653a5396bf40bf70319e
Matt Lilley <m.k.lilley@gmail.com>
2024-11-16 10:48:06 +0000

    Implements checks on boxStatus in DeleteCard

--- src/components/DeleteCard/index.jsx
+++ src/components/DeleteCard/index.jsx
@@ -1,13 +1,20 @@
 import Modal from "../Modal";
 
 import { useSetRecoilState, useRecoilValue } from "recoil";
-import { reloadCardsState, cardToDeleteState } from "@globalState";
+import {
+  reloadCardsState,
+  cardToDeleteState,
+  boxStatusState,
+  useRemoteStorageState,
+} from "@globalState";
 
 import { db } from "../../services/storage";
 
 function DeleteCard({ close }) {
   const setReloadCards = useSetRecoilState(reloadCardsState);
   const card = useRecoilValue(cardToDeleteState);
+  const boxStatus = useRecoilValue(boxStatusState);
+  const useRemoteStorage = useRecoilValue(useRemoteStorageState);
 
   async function deleteCard() {
     await db.delete(card.id, { remote: false });
@@ -19,6 +26,15 @@ function DeleteCard({ close }) {
     <Modal close={close}>
       <h2>Delete card</h2>
       <div className="purple">
+        {!boxStatus && useRemoteStorage && (
+          <>
+            <span className="error">
+              {" "}
+              Problem with online storage. Only local data will be deleted.
+            </span>
+            <br />
+          </>
+        )}
         <button onClick={deleteCard}>Yes, delete card</button>
         <br />
         <br />
