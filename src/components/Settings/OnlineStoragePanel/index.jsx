401a736c529709798e2a195d5518f46cabd9687c
Matt Lilley <m.k.lilley@gmail.com>
2024-11-16 11:48:48 +0000

    Implements recreating cards remotely and locally upon toggle of useRemtoeStorage

--- src/components/Settings/OnlineStoragePanel/index.jsx
+++ src/components/Settings/OnlineStoragePanel/index.jsx
@@ -1,17 +1,84 @@
 import Panel from "../../Panel";
 
 import { useState } from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { useRemoteStorageState, boxIDState, apiKeyState, boxStatusState } from "@globalState";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import {
+  useRemoteStorageState,
+  boxIDState,
+  apiKeyState,
+  boxStatusState,
+  reloadCardsState,
+} from "@globalState";
+
+import { db } from "../../../services/storage";
 
 function OnlineStoragePanel() {
   const [useRemoteStorage, setUseRemoteStorage] = useRecoilState(useRemoteStorageState);
   const boxID = useRecoilValue(boxIDState);
   const apiKey = useRecoilValue(apiKeyState);
   const boxStatus = useRecoilValue(boxStatusState);
+  const setReloadCards = useSetRecoilState(reloadCardsState);
 
   function handleToggleOnlineStorage() {
-    setUseRemoteStorage((prev) => !prev);
+    setUseRemoteStorage((prev) => {
+      const newValue = !prev;
+
+      // if useRemoteStorage has been turned on, we need to delete cards stored locally
+      // and recreate them remotely and vice versa if the user switches off useRemoteStorage.
+      if (newValue) {
+        recreateCardsRemotely();
+      } else {
+        recreateCardsLocally();
+      }
+
+      return newValue;
+    });
+  }
+
+  async function recreateCardsLocally() {
+    // Load cards from the database
+    const cards = await db.read({});
+
+    // We will be deleting all remote cards at once and then recreating them locally.
+    let cardsToCreate = [];
+    for (let card of cards) {
+      cardsToCreate.push({
+        question: card.question,
+        answer: card.answer,
+        flipped: false,
+        reads: card.reads,
+        difficulty: card.difficulty,
+      });
+    }
+    await db.delete(null, { remote: true });
+    await db.create(cardsToCreate, { remote: false });
+
+    // Reload the cards from the data store to update the view
+    setReloadCards((prev) => prev + 1);
+  }
+
+  async function recreateCardsRemotely() {
+    // Load cards from the database
+    const cards = await db.read({});
+
+    // We will be deleting all local cards at once and then recreating them
+    // remotely.
+    let cardsToCreate = [];
+    for (let card of cards) {
+      cardsToCreate.push({
+        question: card.question,
+        answer: card.answer,
+        flipped: false,
+        reads: card.reads,
+        difficulty: card.difficulty,
+      });
+    }
+
+    await db.delete(null, { remote: false });
+    await db.create(cardsToCreate, { remote: true });
+
+    // Reload the cards from the data store to update the view
+    setReloadCards((prev) => prev + 1);
   }
 
   return (
