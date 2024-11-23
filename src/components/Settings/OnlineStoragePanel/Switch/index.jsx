93430181d5c3173eeb5c3f7d0514c4cee24160ca
Matt Lilley <m.k.lilley@gmail.com>
2024-11-23 06:42:28 +0000

    Implements box switching

--- src/components/Settings/OnlineStoragePanel/Switch/index.jsx
+++ src/components/Settings/OnlineStoragePanel/Switch/index.jsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
 
+import { useSetRecoilState } from "recoil";
+import { reloadCardsState, usingMyBoxState } from "@globalState";
+
 import Modal from "../../../Modal";
+
+import { db } from "../../../../services/storage";
 
 function Switch({ close }) {
   const [switchBoxID, setSwitchBoxID] = useState("");
   const [switchApiKey, setSwitchApiKey] = useState("");
   const [switchBoxError, setSwitchBoxError] = useState("");
   const [canEdit, setCanEdit] = useState(false);
+  const setReloadCards = useSetRecoilState(reloadCardsState);
+  const setUsingMyBox = useSetRecoilState(usingMyBoxState);
 
   function handleKeyDown(event) {
     if (event.key === "Enter") {
@@ -19,8 +26,36 @@ function Switch({ close }) {
     setCanEdit((prev) => !prev);
   }
 
-  async function switchBox(options = {}) {
-    console.log("Testing switch flow", switchBoxID, switchApiKey);
+  async function switchBox() {
+    setSwitchBoxError("");
+
+    // User has selected to have use an editable storage box but not given a key
+    if (canEdit && !switchApiKey) {
+      setSwitchBoxError("Error: Storage key must be a valid UUID.");
+      return;
+    }
+
+    // Try to switch to new box. If the boxID isn't valid then we give user error message
+    const switchedOK = await db
+      .switch(switchBoxID.toLowerCase(), switchApiKey.toLowerCase())
+      .catch((error) => {
+        setSwitchBoxError(error.message);
+      });
+
+    if (switchedOK) {
+      setSwitchBoxID("");
+      setSwitchApiKey("");
+
+      // After switching we need to restore the data from the remote source.
+      // Pull the cards data from the remote database and save to local storage
+      await db.read({ remote: true });
+      // Load the cards into view
+      setReloadCards((prev) => prev + 1);
+
+      setUsingMyBox(false);
+
+      close();
+    }
   }
 
   return (
