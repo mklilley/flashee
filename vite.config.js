bedaec44b6f080c8e8503a2bc5e957fcc7404332
Matt Lilley <m.k.lilley@gmail.com>
2025-01-16 10:27:31 +0000

    Adds PWA capability to the app

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,109 @@
 import { defineConfig } from "vite";
-import path from 'path';
+import path from "path";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      strategies: "generateSW",
+      registerType: "prompt",
+      injectRegister: "auto",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2}"],
+      },
+      manifest: {
+        start_url: "./index.html",
+        name: "Flashee",
+        short_name: "Flashee",
+        description: "The free and simple flash card app",
+        themeColor: "#feca34",
+        msTileColor: "#000000",
+        appleMobileWebAppCapable: "yes",
+        appleMobileWebAppStatusBarStyle: "black",
+        icons: [
+          {
+            src: "./assets/img/icons/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "./assets/img/icons/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "./assets/img/icons/android-chrome-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "./assets/img/icons/android-chrome-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "./assets/img/icons/apple-touch-icon-60x60.png",
+            sizes: "60x60",
+            type: "image/png",
+          },
+          {
+            src: "./assets/img/icons/apple-touch-icon-76x76.png",
+            sizes: "76x76",
+            type: "image/png",
+          },
+          {
+            src: "./assets/img/icons/apple-touch-icon-120x120.png",
+            sizes: "120x120",
+            type: "image/png",
+          },
+          {
+            src: "./assets/img/icons/apple-touch-icon-152x152.png",
+            sizes: "152x152",
+            type: "image/png",
+          },
+          {
+            src: "./assets/img/icons/apple-touch-icon-180x180.png",
+            sizes: "180x180",
+            type: "image/png",
+          },
+          {
+            src: "./assets/img/icons/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+          },
+          {
+            src: "./assets/img/icons/favicon-16x16.png",
+            sizes: "16x16",
+            type: "image/png",
+          },
+          {
+            src: "./assets/img/icons/favicon-32x32.png",
+            sizes: "32x32",
+            type: "image/png",
+          },
+          {
+            src: "./assets/img/icons/msapplication-icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "./assets/img/icons/mstile-150x150.png",
+            sizes: "150x150",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   base: "",
   resolve: {
     alias: {
-      '@globalState': path.resolve(__dirname, './src/globalState'),
+      "@globalState": path.resolve(__dirname, "./src/globalState"),
     },
-  }
+  },
 });
