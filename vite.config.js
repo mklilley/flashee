8b88363742ba62cfdf62eaf4c86d17b36414629b
Matt Lilley <m.k.lilley@gmail.com>
2025-01-17 22:37:43 +0000

    Adds compression plugin

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
+import compression from 'vite-plugin-compression';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -99,6 +101,7 @@ export default defineConfig({
         ],
       },
     }),
+    compression()
   ],
   base: "",
   resolve: {
